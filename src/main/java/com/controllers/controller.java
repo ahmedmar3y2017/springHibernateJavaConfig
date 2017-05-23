f2d613231317c88f3a9cc5a151a79a7d23278e76
ahmedmar3y2017 <ahmedmar3y108@gmail.com>
2017-05-23 04:18:35 +0200

    init

--- src/main/java/com/controllers/controller.java
+++ src/main/java/com/controllers/controller.java
@@ -1,17 +1,167 @@
+
+
+
 package com.controllers;
 
+import Entities.student;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.text.SimpleDateFormat;
 
 /**
  * Created by ahmed on 5/22/2017.
  */
 @Controller
 public class controller {
+      /*
+   * -------------------------- using path variable as string -------------------------------
+   *
+   * @RequestMapping("/welcome/{username}") protected ModelAndView
+   * GEtHelloWorld(@PathVariable("username") String username) throws Exception {
+   *
+   * ModelAndView modelAndView = new ModelAndView("hello"); modelAndView.addObject("message",
+   * "First Spring app   : " + username);
+   *
+   * return modelAndView; }
+   */
 
-    @RequestMapping("/")
-    public String GetView(){
-        return "hello";
+  /*
+   * -------------------------------- using path variable as map variable
+   *
+   * @RequestMapping("/welcome/{username}/{address}") protected ModelAndView
+   * GEtHelloWorld(@PathVariable Map<String, String> map) throws Exception {
+   *
+   * String username = map.get("username"); String address = map.get("address");
+   *
+   * ModelAndView modelAndView = new ModelAndView("hello"); modelAndView.addObject("message",
+   * "First Spring app   : " + username + "    :   " + address);
+   *
+   * return modelAndView; }
+   */
+    //
+    // @RequestMapping("/welcome")
+    // protected ModelAndView GEtHelloWorld() throws Exception {
+    //
+    // ModelAndView modelAndView = new ModelAndView("hello");
+    // modelAndView.addObject("message", "First Spring app  : ");
+    //
+    // return modelAndView;
+    // }
+    
+ /*
+ *    @InitBinder
+    public void initBind(WebDataBinder webDataBinder) {
+
+        // webDataBinder.setDisallowedFields(new String[] { "address" });
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy***mm***dd");
+        webDataBinder.registerCustomEditor(java.util.Date.class, "date", new CustomDateEditor(
+                simpleDateFormat, false));
+        webDataBinder.registerCustomEditor(String.class, "username", new studentNameEditor());
+
+    }
+ *
+ * */
+
+
+    // -------------------- to init binder with java class Ex:(format)------------------------
+    @InitBinder
+    public void initBind(WebDataBinder webDataBinder) {
+// ------------- disallaw field ..........
+        webDataBinder.setDisallowedFields(new String[]{"repeat_password"});
+        // ----------- date formate -------------------------
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy***mm***dd");
+        webDataBinder.registerCustomEditor(java.util.Date.class, "birthDate", new CustomDateEditor(
+                simpleDateFormat, false));
+
+        webDataBinder.registerCustomEditor(String.class, "username", new studentNameEditor());
+
+
+    }
+
+    // this instead of creating model object in two methode inside the same class
+    @ModelAttribute
+    public void AllModel(Model model) {
+        model.addAttribute("message", "First Spring app Form ..... : ");
+
+    }
+    // -------------------- welcome Jsp  Page -------------------------------
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView GetView() {
+        ModelAndView modelAndView = new ModelAndView("hello");
+
+        return modelAndView;
+
+    }
+
+    //------------------------ login page  ----------------------------
+    @RequestMapping(value = "/login.html", method = RequestMethod.POST)
+    public ModelAndView LoginHtml(@Valid @ModelAttribute("student") student student, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+
+            ModelAndView modelAndView = new ModelAndView("hello");
+
+            return modelAndView;
+        }
+
+        if (student.getUsername().equalsIgnoreCase("admin") || student.getPassword().equalsIgnoreCase("admin")) {
+            ModelAndView modelAndView = new ModelAndView("hello");
+            modelAndView.addObject("admin", "Canot Sign As Admin");
+
+            return modelAndView;
+
+        }
+        if (student.getUsername().equalsIgnoreCase("ahmed") && student.getPassword().equalsIgnoreCase("123")) {
+
+
+            ModelAndView modelAndView = new ModelAndView("done");
+            modelAndView.addObject("student", student);
+
+            return modelAndView;
+        }
+
+
+        ModelAndView modelAndView = new ModelAndView("signup");
+        modelAndView.addObject("signup", "signUp First Ya Man ");
+
+        return modelAndView;
+
+    }
+
+    @RequestMapping(value = "/sign.html", method = RequestMethod.GET)
+    public ModelAndView sign() {
+
+        ModelAndView modelAndView = new ModelAndView("signup");
+
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/sigup.html", method = RequestMethod.POST)
+    public ModelAndView SignUpAction(@Valid @ModelAttribute("student") student student, BindingResult bindingResult) {
+
+        System.out.println(student.getUsername() + "   --------------- ********************* ---------------------  ");
+        if (bindingResult.hasErrors()) {
+
+            ModelAndView modelAndView = new ModelAndView("signup");
+
+            return modelAndView;
+        }
+
+
+        ModelAndView modelAndView = new ModelAndView("doneSign");
+        modelAndView.addObject("student", student);
+
+
+        return modelAndView;
 
     }
 
